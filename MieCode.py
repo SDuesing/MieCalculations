87f5ca03f84e62af45739e2446d304576507b5db
sduesing <duebasti@gmail.com>
2019-03-27 22:14:04 +0100

    Merge branch 'master' of C:\Users\dueba\PycharmProjects\MieCalculations with conflicts.

--- MieCode.py
+++ MieCode.py
@@ -9,201 +9,98 @@ import PyMieScatt as ps
 from integrate import integrateTrap
 from hygroGrowth import *
 import matplotlib.pyplot as plt
-import math
-import os
-from progressbar import drawProgressBar
-import random
 
 # setup
-mixture = "CS"  # CS for core-shell
+mixture = "HOM" # CS for core-shell
+refIndAerosol = 1.53 + 0.015j
+refIndWater = 1.33 + 1e-6j
 wet = True
 if wet:
     print("wet")
 else:
     print("dry")
-filePathSizeDist = 'X:/p3_working/SDuesing-home/Data/MelCol/20150628/20150628b_lastcorr_smoothed_g0_diffkorr_ab8nm_totKorr_cpceff_inletcorr_final.nsd'
-filePathActos = 'X:\\p3_working\\SDuesing-home\\Data\\MelCol\\20150628\\20150628b_ver2.txt'
-
-fVolBC = 0.012  # 1.2% black carbon content
-numberConcentrationError: float = 0.1  # 10 percent error size distribution
-
-monteCarloIterations = 50
-
-# refractive indices
-realPartWater = 1.33
-realPartBC = 1.75
-realPartSol = 1.53
-
-imPartWater = 1e-6
-imPartBc = 0.55
-imPartSol = 1e-6
-
-RealPartSolError = 0.5  # percent
-RealPartBCError = 4.  # percent
-RealPartWaterError = 0.5  # percent
-
-imPartSolError = 0.  # percent
-imPartBCError = 6.6  # percent
-imPartWaterError = 0.  # percent
-
 # setup end
-
-
-tableSizeDist = np.genfromtxt(
-    filePathSizeDist,
+table = np.genfromtxt(
+    'test.txt',
     delimiter="\t")
-
-file = os.path.basename(filePathSizeDist)
-prefix = file.split(sep=".")[0]
-
-diameter = tableSizeDist[0, :][4:]
-concentrations: float = tableSizeDist[1::2, 4:]
-ScanTimes = tableSizeDist[1::2, 0]
+diameter = table[0, :][4:]
+concentrations: float = table[1::2, 4:]
 numberOfScans = np.ma.shape(concentrations)[0]
 wavelengths = np.array([355, 450, 525, 532, 624, 630, 880, 1064])
 finalCoefficients = np.zeros((numberOfScans, np.ma.shape(wavelengths)[0] * 4), dtype=float)
-finalCoefficientsSd = np.zeros((numberOfScans, np.ma.shape(wavelengths)[0] * 4), dtype=float)
-my_list = list(range(100))
-
-numberOfCalculations = np.ma.shape(concentrations)[0] * diameter.size * wavelengths.size * monteCarloIterations
-print(numberOfCalculations)
-n = 0.
 
 for i in range(0, numberOfScans, 1):
     concOfScan = concentrations[i, :][:]
-    SigmaBackVectorMonte = np.zeros((monteCarloIterations, wavelengths.size), dtype=float)
-    SigmaExtVectorMonte = np.zeros((monteCarloIterations, wavelengths.size), dtype=float)
-    SigmaScaVectorMonte = np.zeros((monteCarloIterations, wavelengths.size), dtype=float)
-    SigmaAbsVectorMonte = np.zeros((monteCarloIterations, wavelengths.size), dtype=float)
-    for monte in range(monteCarloIterations):
-        conc = concOfScan * (1. + random.uniform(-1., 1.) * numberConcentrationError)
-        # generate normal distributed RH from measured values
-        refIndBC = np.random.normal(realPartBC, realPartBC * RealPartBCError / 100., 1) + np.random.normal(
-            imPartBc, imPartBc * imPartBCError / 100., 1) * 1j
-        refIndSol = np.random.normal(realPartSol, realPartSol * RealPartSolError / 100., 1) + np.random.normal(
-            imPartSol, imPartSol * imPartSolError / 100., 1) * 1j
-        if wet:
-            refIndWater = np.random.normal(realPartWater, realPartWater * RealPartWaterError / 100.,
-                                           1) + np.random.normal(
-                imPartWater, imPartWater * imPartWaterError / 100., 1) * 1j
+    countW = 0
+    for w in wavelengths:
+        diameterBin = 0
+        QbackVector = np.zeros((np.ma.shape(concOfScan)[0], 1))
+        QextVector = np.zeros((np.ma.shape(concOfScan)[0], 1))
 
-            # generate normal distributed RH from measured values
-            meanRH = 0.6
-            sdRH = 0.05
-            relHum = np.random.normal(meanRH, sdRH, 1)
+        QscaVector = np.zeros((np.ma.shape(concOfScan)[0], 1))
+        QabsVector = np.zeros((np.ma.shape(concOfScan)[0], 1))
+        for d in diameter:
+            # print(d, w)
+            numberConcentration: float = concOfScan[diameterBin]
+            if wet:
+                # print("wet calculation")
+                fout = "results_wet.txt"
+                fVolWater = wetVolumefraction(kappa=0.3, rh=0.7, ddry=d, temperature=15)
+                refIndWet: Union[complex, Any] = refIndWater * (1. - fVolWater) + refIndAerosol * fVolWater
+                d_wet = wetdiameter(kappa=0.3, rh=0.7, ddry=d, temperature=15)
+                d = d_wet
+                refInd = refIndWet
+            else:
+                refInd = refIndAerosol
+                fout = "results_dry.txt"
+            if mixture=="CS":
+                # result = ps.MieQCoreShell(dCore=d*fVolBC**(1./3.)refInd, d, w, asDict=True)
+            elif mixture == "HOM":
+                result = ps.MieQ(refInd, d, w, asDict=True)
+            QbackVector[diameterBin, 0] = result['Qback'] * np.pi * 1.0 / 4.0 * (
+                    d * 1e-9) ** 2.0 * numberConcentration / (4.0 * np.pi)
+            QabsVector[diameterBin, 0] = result['Qabs'] * np.pi * 1.0 / 4.0 * (
+                    d * 1e-9) ** 2.0 * numberConcentration
+            QscaVector[diameterBin, 0] = result['Qsca'] * np.pi * 1.0 / 4.0 * (
+                    d * 1e-9) ** 2.0 * numberConcentration
+            QextVector[diameterBin, 0] = result['Qext'] * np.pi * 1.0 / 4.0 * (
+                    d * 1e-9) ** 2.0 * numberConcentration
 
-            # generate normal distributed T from measured values
-            meanT = 20
-            sdT = 5
-            T = np.random.normal(meanT, sdT, 1)
+            diameterBin += 1
+        SigmaAbs = integrateTrap(y=QabsVector[:, 0], x=np.ma.transpose(diameter))
+        SigmaExt = integrateTrap(y=QextVector[:, 0], x=np.ma.transpose(diameter))
+        SigmaSca = integrateTrap(y=QscaVector[:, 0], x=np.ma.transpose(diameter))
+        SigmaBack = integrateTrap(y=QbackVector[:, 0], x=np.ma.transpose(diameter))
 
-            # generate normal distributed T from measured values
-            meanKappa = 0.366
-            sdKappa = 0.0121
-            Kappa = np.random.normal(meanKappa, sdKappa, 1)
+        columnStart = 0 + countW * 4
+        columnEnd = 4 + countW * 4
+        coefficients = np.array([[SigmaExt, SigmaSca, SigmaAbs, SigmaBack]])
+        coefficients = coefficients / 1e-6 * 1e6
 
-        QBackVector = np.zeros((np.ma.shape(conc)[0], wavelengths.size), dtype=float)
-        QExtVector = np.zeros((np.ma.shape(conc)[0], wavelengths.size), dtype=float)
-        QScaVector = np.zeros((np.ma.shape(conc)[0], wavelengths.size), dtype=float)
-        QAbsVector = np.zeros((np.ma.shape(conc)[0], wavelengths.size), dtype=float)
+        finalCoefficients[i, columnStart:columnEnd] = coefficients
+        countW += 1
 
-        for countW in range(wavelengths.size):
-            diameterBin = 0
-            for d in diameter:
-                # print(d, w)
-                numberConcentration: float = conc[diameterBin]
-                diameterBC = d * fVolBC ** (1. / 3.)
-                if wet:
-                    d_wet = wetdiameter(kappa=Kappa, rh=relHum, ddry=d, temperature=T)
-                    dParticle = d_wet
-                    volumeWater = 1. / 6. * np.pi * (math.pow(d_wet, 3.) - math.pow(d, 3.))
-                    volumeSol = np.pi * 1. / 6. * (math.pow(d, 3.) - math.pow(diameterBC, 3.))
-                    volumeBC = fVolBC * 1. / 6. * np.pi * math.pow(d, 3.)
-                    totalVolume = np.pi * 1. / 6. * math.pow(d_wet, 3.)
-                    # print("Total Volume - Sum of volumes: ", totalVolume - volumeWater - volumeSol - volumeBC)
-                    if mixture == "CS":
-                        fout = prefix + "_" + str(monteCarloIterations) + "_wet_CS"
-                        mShell = volumeSol / (volumeWater + volumeSol) * refIndSol + (
-                                1 - volumeSol / (volumeWater + volumeSol)) * refIndWater
-                        mCore = refIndBC
-                        # print(mShell)
-                    elif mixture == "HOM":
-                        fout = prefix + "_" + str(monteCarloIterations) + "_wet_HOM"
-                        fVoldry = wetVolumefraction(kappa=0.3, rh=0.7, ddry=d, temperature=15)
-                        refIndAerosol = fVolBC * refIndBC + (1. - fVolBC) * refIndSol
-                        refIndWet: Union[complex, Any] = refIndWater * (1. - fVoldry) + refIndAerosol * fVoldry
-                        refInd = refIndWet
+    np.savetxt(fname=fout, X=finalCoefficients, delimiter="\t")
 
-                else:
-                    dParticle = d
-                    if mixture == "CS":
-                        fout = prefix + "_" + str(monteCarloIterations) + "_dry_CS"
-                        mShell = refIndSol
-                        mCore = refIndBC
+kappa = np.linspace(0.1, 0.4, 40)
+diameter = np.linspace(0.1, 5.0, 500)
+result = np.zeros((diameter.shape[0] * kappa.shape[0], 3))
 
-                    elif mixture == "HOM":
-                        refInd = fVolBC * refIndBC + (1. - fVolBC) * refIndSol
-                        fout = prefix + "_" + str(monteCarloIterations) + "_dry_HOM"
+count = 0
+for k in kappa:
+    for d in diameter:
+        d_wet = wetdiameter(d, k, 20, 0.9) / d
+        row = np.array([k, d, d_wet])
+        result[count, 0:3] = np.transpose(row)
+        count += 1
+np.savetxt(fname='d_wet_table.txt', X=result, delimiter="\t")
 
-                if mixture == "CS":
-                    result = ps.MieQCoreShell(dCore=diameterBC, mShell=mShell, mCore=mCore, dShell=d,
-                                              wavelength=wavelengths[countW], asDict=True)
-                elif mixture == "HOM":
-                    result = ps.MieQ(refInd, diameter=dParticle, wavelength=wavelengths[countW], asDict=True)
-
-                QBackVector[diameterBin, countW] = result['Qback'] * np.pi * 1.0 / 4.0 * (
-                        dParticle * 1e-9) ** 2.0 * numberConcentration / (4.0 * np.pi)
-                QAbsVector[diameterBin, countW] = result['Qabs'] * np.pi * 1.0 / 4.0 * (
-                        dParticle * 1e-9) ** 2.0 * numberConcentration
-                QScaVector[diameterBin, countW] = result['Qsca'] * np.pi * 1.0 / 4.0 * (
-                        dParticle * 1e-9) ** 2.0 * numberConcentration
-                QExtVector[diameterBin, countW] = result['Qext'] * np.pi * 1.0 / 4.0 * (
-                        dParticle * 1e-9) ** 2.0 * numberConcentration
-                #print(QExtVector)
-                drawProgressBar(n / numberOfCalculations, barLen=20)
-                n += 1
-                diameterBin += 1
-                #countW += 1
-
-            for nWavelengths in range(wavelengths.size):
-                SigmaAbs = integrateTrap(y=QAbsVector[:, nWavelengths], x=np.ma.transpose(diameter))
-                SigmaExt = integrateTrap(y=QExtVector[:, nWavelengths], x=np.ma.transpose(diameter))
-                SigmaSca = integrateTrap(y=QScaVector[:, nWavelengths], x=np.ma.transpose(diameter))
-                SigmaBack = integrateTrap(y=QBackVector[:, nWavelengths], x=np.ma.transpose(diameter))
-                SigmaBackVectorMonte[monte, nWavelengths] = SigmaBack
-                SigmaExtVectorMonte[monte, nWavelengths] = SigmaExt
-                #print("SigmaExtMonteMatrix Monte durchlauf" + str(monte))
-                #print(SigmaExtVectorMonte)
-                SigmaScaVectorMonte[monte, nWavelengths] = SigmaSca
-                SigmaAbsVectorMonte[monte, nWavelengths] = SigmaAbs
-
-    coefficients = []
-    coefficientsSd = []
-    for nWavelengths in range(wavelengths.size):
-        SigmaExtMean = np.mean(SigmaExtVectorMonte[:, nWavelengths]) / 1.e-6 * 1.e6
-        coefficients.append(SigmaExtMean)
-        SigmaScaMean = np.mean(SigmaScaVectorMonte[:, nWavelengths]) / 1.e-6 * 1.e6
-        coefficients.append(SigmaScaMean)
-        SigmaAbsMean = np.mean(SigmaAbsVectorMonte[:, nWavelengths]) / 1.e-6 * 1.e6
-        coefficients.append(SigmaAbsMean)
-        SigmaBackMean = np.mean(SigmaBackVectorMonte[:, nWavelengths]) / 1.e-6 * 1.e6
-        coefficients.append(SigmaBackMean)
-
-        SigmaExtMean = np.std(SigmaExtVectorMonte[:, nWavelengths]) / 1.e-6 * 1.e6
-        coefficientsSd.append(SigmaExtMean)
-        SigmaScaMean = np.std(SigmaScaVectorMonte[:, nWavelengths]) / 1.e-6 * 1.e6
-        coefficientsSd.append(SigmaScaMean)
-        SigmaAbsMean = np.std(SigmaAbsVectorMonte[:, nWavelengths]) / 1.e-6 * 1.e6
-        coefficientsSd.append(SigmaAbsMean)
-        SigmaBackMean = np.std(SigmaBackVectorMonte[:, nWavelengths]) / 1.e-6 * 1.e6
-        coefficientsSd.append(SigmaBackMean)
-    #print(coefficients)
-    finalCoefficients[i, :] = coefficients
-    finalCoefficientsSd[i, :] = coefficients
-
-
-np.savetxt(fname=fout+"_SD.txt", X=finalCoefficientsSd, delimiter="\t")
-np.savetxt(fname=fout+".txt", X=finalCoefficients, delimiter="\t")
-
-plt.plot(ScanTimes, finalCoefficients[:, 0], "bo")
+x = result[:, 0]
+y = result[:, 1]
+z = result[:, 2]
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+cmhot = plt.get_cmap("hot")
+c = z
+ax.scatter(x, y, z, zdir='z', c=c, cmap=cmhot)
 plt.show()
